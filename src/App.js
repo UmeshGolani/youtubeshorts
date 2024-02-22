c2df9a7957149bbbf94e9708f05718c4655dfd6f
UmeshGolani <ssutg97@gmail.com>
2024-02-22 22:25:57 +0530

    video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Video from './Components/Video';
 
 function App() {
   return (
     <div className="App">
       <h1>Youtube Shorts</h1>
+      <Video/>
     </div>
   );
 }
